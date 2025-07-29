1b5e2b634ba5b3c2db6bb30e5767bbab878727c5
Ana Carolina Silva <ana.carolina3551@gmail.com>
2025-07-28 22:32:10 -0300

    Criação da base do projeto, correção de erros de importação

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from "./Componentes/Header.jsx";
+import Dados from "./Componentes/Dados.jsx";
+import React from 'react';
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-stone-900">
+      <Header />
+      <Dados />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="flex items-center bg-stone-900 h-[150px]">
+            <img src="/logo.png" alt="Logo" className="w-[120px] mx-6"/>
+            <h1 className="text-stone-50 text-5xl ">Organizae</h1>
+        </header>
+    );
+
+}
+
+export default Header;
